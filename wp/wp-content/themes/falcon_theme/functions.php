4cad0be03fc582de20a022a00473fb76dc7b9aa9
David Gatica <davgatik@gmail.com>
2017-10-20 13:10:23 -0500

    18. Agregando widgets 20/10/2017

--- wp/wp-content/themes/falcon_theme/functions.php
+++ wp/wp-content/themes/falcon_theme/functions.php
@@ -17,6 +17,28 @@ add_image_size('slider_thumbs', 470, 300, true); //tamano de imagen en sidebar
 add_image_size('list_articles_thumbs', 450, 370, true);//tamano de imagen en la lista
 
 
+/*------Agregando sidebar---------------*/
+
+register_sidebar(array(
+	'name'=>"Sidebar",
+	'before_widget' => "<section class='widget'>",
+	'after_widget' => "</section>",
+	'before_title' => "<h3>",
+	'after_title' => "</h3>"
+	));
+/*En esta parte del codigo se declara cual es el area de widgets y estara encerrado con una etiqueta section y clase widget y los titulos tendran un H3*/
+
+
+/*------------Lo mismo para footer----------*/
+
+
+register_sidebar(array(
+	'name'=>"Footer",
+	'before_widget' => "<section class='widget'>",
+	'after_widget' => "</section>",
+	'before_title' => "<h3>",
+	'after_title' => "</h3>"
+	));
 
  ?>
 
