1cb245f1a0354d47baa6076eb6ef60a5dbd13f47
David Gatica <davgatik@gmail.com>
2017-11-23 16:05:27 -0600

    Creacion Plug 3. configurando plugin archivo inicial

---
+++ wp/wp-content/themes/falcon_theme/History/history.php
@@ -0,0 +1,12 @@
+<?php 
+/* Cabecera
+Plugin Name: C7 History Developer
+Plugin URI: http://davidgatica.esy.es
+Description: Have a history of what you are building in the project.
+Version: 1.0
+Author: David Gatica
+Autor URI: http://davidgatica.esy.es
+*/
+ ?>
+
+ 
