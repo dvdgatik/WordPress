c2209b682af4d2e2c3338d8f95705cc778beda28
David Gatica <davgatik@gmail.com>
2018-03-09 17:26:43 -0600

    git reset --soft HEAD~

--- wp/wp-content/plugins/History/history.php
+++ wp/wp-content/plugins/History/history.php
@@ -1,7 +1,7 @@
 <?php 
 /* Cabecera
 Plugin Name: C7 History Developer
-Plugin URI: http://localhost/WordPress/wp
+Plugin URI: http://davidgatica.esy.es
 Description: Have a history of what you are building in the project.
 Version: 1.0
 Author: David Gatica
