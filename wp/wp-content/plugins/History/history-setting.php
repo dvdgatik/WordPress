8f7ed5becef0c957575dfbb35495bf6a23d8b676
dvdgatik <davgatik@gmail.com>
2018-10-03 06:40:10 -0500

    5. agregando estilos y scripts

--- wp/wp-content/plugins/History/history-setting.php
+++ wp/wp-content/plugins/History/history-setting.php
@@ -4,8 +4,51 @@ function c7hd_customer_options() {
 	add_options_page('History Developer','History','manage_options','c7hd','c7hd_settings_page');
 }
 
-function c7hd_settings_page() {
- echo '<h1>History Developer<h1>';
+function c7hd_settings_page() { ?>
+ <h1>History Developer<h1>
+ <p>Listado de notas para desarrolladores</p>
+
+ <form action="index.html" method="post">
+ 	<div class="c7-form-group">
+ 		<label for="c7_note">Ingresar tu nota</label>
+ 		<textarea name="c7_note" id="c7_note" class="c7-input"></textarea>
+ 	</div>
+ </form>
+ <h2>Listado de notas</h2>
+
+ <div class="c7-nota">
+ 	<p>Mi primera nota</p>
+ 	<p>7 de abril del 2017</p>
+ </div>
+  <div class="c7-nota">
+ 	<p>Mi primera nota</p>
+ 	<p>7 de abril del 2017</p>
+ </div>
+  <div class="c7-nota">
+ 	<p>Mi primera nota</p>
+ 	<p>7 de abril del 2017</p>
+ </div>
+  <div class="c7-nota">
+ 	<p>Mi primera nota</p>
+ 	<p>7 de abril del 2017</p>
+ </div>
+
+ <?php
+
 }
+
+
+
+/*
+options.php: archivo que recibe todas las opciones del
+panel de administracion
+
+Es importante poner un alias para no modificar los estilos
+de otros plugins
+
+Crear carpeta para estilos y js "assets"
+
+Recordar que los plugins son escalables se le pueden añadir mas cosas mas adelante por eso hay que llevar un orden
+*/
 
  ?>
--- wp/wp-content/plugins/History/history.php
+++ wp/wp-content/plugins/History/history.php
@@ -9,6 +9,22 @@ Autor URI: http://davidgatica.esy.es
 */
 define('C7HD_ROOTDIR', plugin_dir_path(__FILE__));
 define('C7HD_URLPLUGIN', plugin_dir_url(__FILE__));
+
+//Funcion para agregar CSS
+/*parametros
+alias
+ruta
+false header
+true footer
+*/
+function c7_admin_assets() {
+	wp_enqueue_style('c7_admin_css', C7HD_URLPLUGIN .'assets/css/main.css', false, '1.0');
+}
+
+add_action('admin_enqueue_scripts', 'c7_admin_assets');
+//agrega la funcion de estilos justo cuando carga los scripts con el hook
+
+//hook ejecutar funciones cuando WordPress haga algo
 require_once(C7HD_ROOTDIR.'history-setting.php');
 
  ?>
