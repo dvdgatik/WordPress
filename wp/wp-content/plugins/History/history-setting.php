fdf92096b15409cdf4986566e39b4aeabb1d5969
dvdgatik <davgatik@gmail.com>
2018-10-04 14:41:16 -0500

    7. Guardando multiples datos en los options

--- wp/wp-content/plugins/History/history-setting.php
+++ wp/wp-content/plugins/History/history-setting.php
@@ -15,48 +15,58 @@ function c7hd_settings_page() { ?>
  	//tiene que estar adentro de la etiqueta form 
  	//Creamos un grupo de campos que estaran guradados bajo esta funcion
  	?>
-
+ 	<?php $numero_nota = get_option('c7_note_numero'); 
+ 			if(empty($numero_nota)) $numero_nota = 0;
+ 		?>
+ 	<input type="hidden" name="c7_note_numero" value="<?php echo ($numero_nota + 1); ?>">
  	<div class="c7-form-group">
  		<label for="c7_note">Ingresar tu nota</label>
- 		<textarea name="c7_note" id="c7_note" class="c7-input"></textarea>
+ 		<textarea name="c7_notes_<?php echo $numero_nota; ?>[nota]" id="c7_note" class="c7-input"></textarea>
+ 		<input type="hidden" name="c7_notes_<?php echo $numero_nota ?>[date]" value="<?php echo date('d-m-y H:i'); ?>">
+ 		<?php 
+ 		/*
+ 		Podemos serializar los datos
+		Esto quiere decir que WordPress nos permite guardar los datos en un array y ese array se guarda en un solo campo
+		en la tabla options
+		WordPress los serializa
+		y despues los deserializa
+ 		*/
+ 		 ?>
  	</div>
  	<?php submit_button(); ?>
  </form>
  <?php
-  $nota= get_option("c7_note"); 
-  echo "Nota: ".$nota;
-  ?>
- <h2>Listado de notas</h2>
+  //$nota= get_option("c7_note"); 
+  //echo "Nota: ".$nota;
 
- <div class="c7-nota">
- 	<p>Mi primera nota</p>
- 	<p>7 de abril del 2017</p>
- </div>
-  <div class="c7-nota">
- 	<p>Mi primera nota</p>
- 	<p>7 de abril del 2017</p>
- </div>
-  <div class="c7-nota">
- 	<p>Mi primera nota</p>
- 	<p>7 de abril del 2017</p>
- </div>
-  <div class="c7-nota">
- 	<p>Mi primera nota</p>
- 	<p>7 de abril del 2017</p>
- </div>
+  /*Imprimir como si fuera un array*/
+  //print_r($nota);
+  ?>
+
+<?php if($numero_nota > 0) { ?>
+	<h2>Listado de notas</h2>
+	<?php for ($i=($numero_nota-1); $i >= 0 ; $i= $i-1) {
+		$nota = get_option('c7_notes_'.$i); 
+		?>
+	 <div class="c7-nota">
+	 	<p><?php echo $nota['nota'];?></p>
+	 	<p><?php echo $nota['date']; ?></p>
+	 </div>
+	<?php } ?>
+
+
 
  <?php
+}
 
 }
 
-//Crear una funcion que se encargue de  guardar cada uno de los datos
+//Crear una funcion que se encargue de  guardar cada dato
 
 function c7_customer_register_settings() {
-	register_setting('c7_fields_options', 'c7_note'); //se encarga de registrar cambios en la base de datos
-	/*
-		primer parametro: grupo que creaste dentro del form
-		segundo parametro: nombre del campo en este caso del textarea
-	*/
+	register_setting('c7_fields_options', 'c7_note_numero'); //se encarga de registrar cambios en la base de datos
+	$numero_nota = get_option('c7_note_numero'); 
+	register_setting('c7_fields_options', 'c7_notes_'.$numero_nota);
 }
 // hook para almacenarlo
 add_action('admin_init', 'c7_customer_register_settings');
